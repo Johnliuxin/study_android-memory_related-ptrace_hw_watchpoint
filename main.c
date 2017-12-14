ef01c893446ba3071071e583c9856e31530a5d75
John.LiuXin <emailofliuxin@126.com>
2017-12-14 19:55:18 +0800

    add parameter parse
    Signed-off-by: John.LiuXin <emailofliuxin@126.com>

--- main.c
+++ main.c
@@ -16,9 +16,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>  //for getopt_long
+
+const char* my_version = "1.0";
 
 int main(int argc, char *argv[])
 {
-    fprintf(stderr, "Done\n");
+    opterr = 0;
+    do {
+        int c;
+        int option_index = 0;
+        static const struct option long_options[] = {
+          { "version",       no_argument,       NULL,   'v' },
+          { "showint",       required_argument, NULL,   's' },
+          { NULL,            0,                 NULL,   0 }
+        };
+
+        c = getopt_long(argc, argv, "vs:", long_options, &option_index);
+
+        if (c == EOF) {
+            break;
+        }
+
+        switch (c) {
+		case 'v':
+            printf("version %s\n", my_version);
+            break;
+        case 's':
+            printf("show value : %d\n", atoi(optarg));
+            break;
+        case '?':
+            fprintf(stderr, "usage: %s -v [-s intValue]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    } while (1);
+
+    if(argc > optind + 1) {
+        fprintf(stderr, "%s: too many arguments\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
